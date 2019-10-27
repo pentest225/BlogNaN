be5906b2a8c97be7ae66a205dfd7e6dc84a0a6e1
Mamoutou <mamoutou.doumbia@uvci.edu.ci>
2019-10-27 23:09:38 +0000

    auth

--- NanBlog/Utilisateurs/views.py
+++ NanBlog/Utilisateurs/views.py
@@ -3,11 +3,12 @@ from .models import MyUser, Specialite
 from django.contrib.auth.models import AbstractUser,Group,Permission
 from rest_framework import viewsets
 from .serializer import MyUserSerializer, SpecialiteSerializer
-# from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework_api_key.permissions import HasAPIKey
 from django.http import	JsonResponse
 import json
 from rest_framework import filters
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -17,12 +18,12 @@ class DynamicSearchFilter(filters.SearchFilter):
     
 class UsersViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = MyUserSerializer
     queryset = MyUser.objects.all()
     
 class SpecialiteViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = SpecialiteSerializer
     queryset = Specialite.objects.all()
--- NanBlog/blogApp/views.py
+++ NanBlog/blogApp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Categorie, Article, Archive, Tag, Commentaire, ResponseCommentaire, Like
 from rest_framework import viewsets
 from .serializer import CategorieSerializer, ArticleSerializer, ArchiveSerializer, TagSerializer, CommentaireSerializer, ResponseCommentaireSerializer, LikeSerializer
-# from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework_api_key.permissions import HasAPIKey
 from django.http import	JsonResponse
 import json
 from rest_framework import filters
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 class DynamicSearchFilter(filters.SearchFilter):
@@ -15,43 +17,43 @@ class DynamicSearchFilter(filters.SearchFilter):
     
 class LikeViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
     
 class ResponseCommentaireViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = ResponseCommentaireSerializer
     queryset = ResponseCommentaire.objects.all()
     
 class CommentaireViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = CommentaireSerializer
     queryset = Commentaire.objects.all()
     
 class ArchiveViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = ArchiveSerializer
     queryset = Archive.objects.all()
     
 class TagViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
     
 class ArticleViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     
 class CategorieViewset(viewsets.ModelViewSet):
     filter_backends = (DynamicSearchFilter,)
-    # permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [HasAPIKey | IsAuthenticated]
     serializer_class = CategorieSerializer
     queryset = Categorie.objects.all()
 
