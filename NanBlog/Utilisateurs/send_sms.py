59dd358bf5a52388d71b0b04a9f60c76e9ac10c7
Sedrickgael <kanangamansedrickgael@gmail.com>
2019-10-29 12:06:44 +0000

    Update send_sms.py

--- NanBlog/Utilisateurs/send_sms.py
+++ NanBlog/Utilisateurs/send_sms.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC87bcdc10483708c4d174405099e1bedd'
-auth_token = '5b0fd2340344516a0f43bf8ca5deb864'
+account_sid = 'AC193493935fdba4e641054d7139783541'
+auth_token = '050252cd07243b778449beb7ffaa038d'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+12057053851',
+                     from_='+12026880556',
                      to='+22508432050'
                  )
 
