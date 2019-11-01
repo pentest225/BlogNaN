dbcaaaac31dfb24d9ab67359d9bd1f58e83e19e7
Sedrickgael <kanangamansedrickgael@gmail.com>
2019-11-01 11:00:32 +0000

    ok

--- NanBlog/Utilisateurs/forms.py
+++ NanBlog/Utilisateurs/forms.py
@@ -1,8 +1,26 @@
+# from django import forms
+# from .models import *
+# from django.contrib.auth.models import Group
+# from django.forms import ModelMultipleChoiceField
+
+# from django.forms.widgets import CheckboxSelectMultiple
+
 from django import forms
-from .models import *
-from django.contrib.auth.models import Group
+from django.contrib.auth.forms import UserCreationForm
+from . import models
+class RegistrationForm(UserCreationForm):
 
-
+    class Meta:
+        model = models.MyUser
+        fields = (
+            'username',
+            'email',
+            'groups',
+            'first_name',
+            'last_name',
+            'description',
+            'image',
+            )
 # class RegistrationForm(forms.ModelForm):
 #     class Meta:
 #         model = MyUser
@@ -22,30 +40,41 @@ from django.contrib.auth.models import Group
 #         user.groups.add(g)
 #         user.save()
 
-from allauth.account.forms import SignupForm
-class RegistrationForm(SignupForm, forms.ModelForm):
+# from allauth.account.forms import SignupForm
+# class RegistrationForm(SignupForm):
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField()
+#     groups = ModelMultipleChoiceField(
+#         queryset=Group.objects.order_by('name'),
+#         widget=CheckboxSelectMultiple,
+#     )
+#     # image = models.FileField(upload_to='image/user/profile/',  blank=True, null=True)
+#     description = forms.CharField(widget=forms.Textarea)
 
-    class Meta:
-        model = MyUser
-        fields =(
-			'groups',
-            'first_name',
-            'last_name',
-            'description',
-		)
-        labels = {'groups': 'Type de compte', 'description': 'Votre Bio'}
-    def save(self, request):
-        # group = self.cleaned_data['groups']
-        # g = Group.objects.get(name=group[0])
-        # print(g)
-        # Ensure you call the parent class's save.
-        # .save() returns a User object.
-        user = super(RegistrationForm, self).save(request)
 
-        # Add your own processing here.
+#     class Meta:
+#         model = MyUser
+#         labels = {'groups': 'Type de compte', 'description': 'Votre Bio'}
+#     def signup(self, request, user):
+#         user.first_name = self.cleaned_data['first_name']
+#         user.last_name = self.cleaned_data['last_name']
+#         user.groups = self.cleaned_data['groups']
+#         # user.image = self.cleaned_data['image']
+#         user.description = self.cleaned_data['description']
+#         user.save()
+#         return user
+    # def save(self, request):
+    #     # group = self.cleaned_data['groups']
+    #     # g = Group.objects.get(name=group[0])
+    #     # print(g)
+    #     # Ensure you call the parent class's save.
+    #     # .save() returns a User object.
+    #     user = super(RegistrationForm, self).save(request)
 
-        # You must return the original result.
-        return user
+    #     # Add your own processing here.
+
+    #     # You must return the original result.
+    #     return user
 # class EtablissementRegistrationForm(UserCreationForm):
 # 	class Meta:
 # 		model=EtablissementUser
--- NanBlog/Utilisateurs/urls.py
+++ NanBlog/Utilisateurs/urls.py
@@ -7,8 +7,11 @@ router = DefaultRouter()
 router.register(r'user', UsersViewset, basename='user')
 router.register(r'special', SpecialiteViewset, basename='special')
 
-from . import views
+from . import view
 
 urlpatterns = [
+    path('inscription/', view.inscription, name='inscription'),
+    path('verification/', view.signup_sendmail, name='sign_send_verif'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', view.activate, name='activate')
 ]
 urlpatterns += router.urls
