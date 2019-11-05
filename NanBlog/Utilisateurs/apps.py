7094a1e1d8efc3a4d127573f25c93fe33364b1a0
guindosaros <47152090+guindosaros@users.noreply.github.com>
2019-11-05 15:36:28 +0000

    reglage

--- NanBlog/Utilisateurs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UtilisateursConfig(AppConfig):
-    name = 'Utilisateurs'
--- NanBlog/allConfig/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AllconfigConfig(AppConfig):
-    name = 'allConfig'
