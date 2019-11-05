98ec422ce6023ea18bf33e5d4ce24481449f145d
Reine <guelanin.koulai@uvci.edu.ci>
2019-11-05 19:24:37 +0000

    insertion du dashboard
    insertion

--- NanBlog/blogApp/views.py
+++ NanBlog/blogApp/views.py
@@ -12,6 +12,30 @@ from rest_framework import filters
 from rest_framework.response import Response
 from .form import ArticleFrom
 
+
+
+
+##################################### import Dashboard groupe 1 ###################################
+
+
+
+from django.shortcuts import render, redirect
+# from blogger.models import *
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+
+
+
+
+
+####################################################################################################
+
+
+
 # Create your views here.
 
 class DynamicSearchFilter(filters.SearchFilter):
@@ -270,3 +294,211 @@ def addarticle(request):
 #              PAGE VISITEUR                   #
 #                                              #
 ################################################
+
+
+
+
+
+
+
+
+
+
+
+
+############################################# Dashboard groupe 1 ###########################################
+
+
+@login_required(login_url='login')
+def index_dash(request):
+    # catego= Article.objects.filter(categorie__pk = pk ).order_by('-id')
+    # categorie__id = pk
+    # profil= Profile.objects.all()[:1]
+    # comment= Commentaire.objects.filter(statut=True, article__valider=True)
+    # catego= Category.objects.filter(statut=True)
+    # article= Article.objects.filter(statut=True)
+    # articl= Article.objects.filter(statut=True, valider=True )
+    # articles= Article.objects.filter(statut=True, valider=True )
+    # articlee= Article.objects.filter(statut=True, valider=False )
+    # context = {"catego": catego, "comment": comment, "profil": profil, "article": article, "articles": articles, "articlee": articlee,}
+    
+    return render(request, 'pages/index_dash.html')
+
+@login_required(login_url='connecter')
+def admin_visiteur_dash(request):
+    # article = Article.objects.filter(statut=True, valider=True)
+    # commentaires = Commentaire.objects.filter(statut=True, article__valider=True)
+    # data = {
+    #     'commentaire': commentaires,
+    #     'article':article,
+    # }
+    return render(request, 'pages/admin_visiteur_dash.html')
+
+
+def connect(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+        _next = request.GET.get('next', False)
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None and user.is_active:
+            
+            print("user is login")
+
+            login(request, user)
+            if _next: 
+                return redirect(_next)
+            else:
+                return redirect('index_dash')
+        else:
+            return render(request, 'pages/connexion.html')
+    return render(request, 'pages/connexion.html')
+
+def connecter(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+        _next = request.GET.get('next', False)
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None and user.is_active:
+            
+            print("user is login")
+
+            login(request, user)
+            if _next: 
+                return redirect(_next)
+            else:
+                return redirect('admin_visiteur_dash')
+        else:
+            return render(request, 'pages/connexion.html')
+    return render(request, 'pages/connexion.html')
+
+
+
+
+def deconection(request):
+    logout(request)
+    return redirect('connect')
+
+def deconexion(request):
+    logout(request)
+    return redirect('connecter')
+
+
+
+
+def detail_visiteur_dash(request, id):
+    # selectarticle = Article.objects.get(pk=id)
+    # commentaire = Commentaire.objects.filter(article__id = id, article__valider=True)
+    # profil= Profile.objects.all()[:1]
+    # context = {
+    #     "profil": profil,
+    #     'selectarticle':selectarticle,
+    #     'commentaire':commentaire,
+    #     }
+    return render(request, 'pages/detaill_visiteur_dash.html')
+
+def page_dash(request):
+    return render(request, 'pages/page_dash.html')
+
+def post_attend_dash(request):
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # articlee= Article.objects.filter(statut=True, valider=False )
+    # context = {"articlee": articlee, "commente": commente, "profil": profil,}
+    return render(request, 'pages/post_attend_dash.html')
+
+def post_partage_dash(request):
+    # profil= Profile.objects.all()[:1]
+    # context = {"profil": profil,"commente": commente,}
+    return render(request, 'pages/post_partage_dash.html')
+
+def post_valid_dash(request):
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # articles= Article.objects.filter(statut=True, valider=True )
+    # context = {"articles": articles, "profil": profil,}
+    
+    return render(request, 'pages/post_valid_dash.html')
+
+def profil_visiteur_dash(request):
+    return render(request, 'pages/profil_visiteur_dash.html')
+
+def form_article_dash(request):
+    # message = ''
+    # catego= Category.objects.filter(statut=True)
+    # prof= Profile.objects.all()
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # context = {"commente": commente, "profil": profil,"prof": prof,"catego": catego}
+    
+    # if request.method == "POST":
+    #     titre = request.POST.get('titre')
+    #     user_id = request.POST.get('username')
+    #     # user = User.objects.get(pk=5)
+    #     categorie_id = request.POST.get('categorie')
+    #     categorie = Category.objects.get(pk=2)
+    #     image = request.FILES.get('image')
+    #     description = request.POST.get('description')
+    #     content = request.POST.get('content')
+    #     #print("Titre=",titre,"userid=",user_id,"categori_id=",categorie_id,"image=",image,'description=',description,'content=',content)
+    #     # print('user=',user)
+    #     print('categorie=', categorie)
+        
+    #     article = Article()
+    #     article.titre = titre
+    #     # article.user_id = user
+    #     article.categorie = categorie
+    #     article.image = image
+    #     article.description = description
+    #     article.valider = False
+    #     article.content = content
+    #     article.save()
+    #     #message="Article enregistré avec succes !"
+        
+    #     #message="Probleme d'enregistrement!"
+    #     #print('error enregistrement')
+
+    # context = {
+    #     "commente": commente, 
+    #     "profil": profil,
+    #     "prof": prof,
+    #     "catego": catego,
+    #     'message':message,
+    # }
+
+
+    return render(request, 'pages/form_article_dash.html')
+
+def form_profil_dash(request):
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # context = {"commente": commente,"profil": profil, "profil": profil,}
+    return render(request, 'pages/form_profil_dash.html')
+
+def project_detail_dash(request, pk):
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # article= Article.objects.filter(statut=True)
+    # art = Article.objects.get(pk=pk)
+    # comment = Commentaire.objects.filter(article__valider = True, article__pk = pk).order_by('-id')
+    # context = {"article": article,"profil": profil, "commente": commente, "art": art, "comment": comment,}
+    return render(request, 'pages/project_detail_dash.html')
+
+def tables_dash(request):
+    # profil= Profile.objects.all()[:1]
+    # commente= Commentaire.objects.filter(statut=True, article__valider=True)
+    # article= Article.objects.filter(statut=True)
+    # context = {"article": article, "commente": commente,"profil": profil,}
+    return render(request, 'pages/tables_dash.html')
+
+def tables_visiteur_dash(request):
+    # article= Article.objects.filter(statut=True)
+    # data = {
+    #     'article':article,
+    # }
+    return render(request, 'pages/tables_visiteur_dash.html')
