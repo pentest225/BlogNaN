4304aa6061c06db0442a498853bcc6c59b0a8114
guindosaros <47152090+guindosaros@users.noreply.github.com>
2019-11-09 03:06:29 +0000

    Customisation de la partie admin

--- NanBlog/Contacts/apps.py
+++ NanBlog/Contacts/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ContactsConfig(AppConfig):
     name = 'Contacts'
+    verbose_name = "Panel des Messages et Newsletters"
--- NanBlog/allConfig/admin.py
+++ NanBlog/allConfig/admin.py
@@ -1,21 +1,18 @@
 # vim: set fileencoding=utf-8 :
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from . import models
 
 
 class AllInfoAdmin(admin.ModelAdmin):
 
     list_display = (
-        'id',
         'titre',
         'phone',
         'ville',
         'commune',
         'email',
-        'description',
         'contactText',
-        'icon',
         'status',
         'date_add',
         'date_upd',
@@ -24,90 +21,106 @@ class AllInfoAdmin(admin.ModelAdmin):
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'titre',
-        'phone',
-        'ville',
-        'commune',
-        'email',
-        'description',
-        'contactText',
-        'icon',
-        'status',
-        'date_add',
-        'date_upd',
     )
+    search_fields = ('titre',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['titre','ville']
+    list_per_page = 10
+    actions = ('active','desactive')
+    ordering = ['titre']
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
 
 
-class workingHoursAdmin(admin.ModelAdmin):
+# class workingHoursAdmin(admin.ModelAdmin):
 
-    list_display = (
-        'id',
-        'day',
-        'openHours',
-        'closeHours',
-        'status',
-        'date_add',
-        'date_upd',
-    )
-    list_filter = (
-        'status',
-        'date_add',
-        'date_upd',
-        'id',
-        'day',
-        'openHours',
-        'closeHours',
-        'status',
-        'date_add',
-        'date_upd',
-    )
+#     list_display = (
+#         'id',
+#         'day',
+#         'openHours',
+#         'closeHours',
+#         'status',
+#         'date_add',
+#         'date_upd',
+#     )
+#     list_filter = (
+#         'status',
+#         'date_add',
+#         'date_upd',
+#         'id',
+#         'day',
+#         'openHours',
+#         'closeHours',
+#         'status',
+#         'date_add',
+#         'date_upd',
+#     )
 
 
 class HeaderFrontAdmin(admin.ModelAdmin):
 
     list_display = (
-        'id',
-        'logo',
-        'image',
+        'affiche_logo',
+        'titre',
         'index_title',
         'about_title',
         'cat_title',
         'blog_title',
         'contct_title',
-        'titre',
         'status',
         'date_add',
         'date_upd',
+        'affiche_image',
     )
     list_filter = (
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'logo',
-        'image',
-        'index_title',
-        'about_title',
-        'cat_title',
-        'blog_title',
-        'contct_title',
-        'titre',
-        'status',
-        'date_add',
-        'date_upd',
     )
+    search_fields = ('titre',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['titre','affiche_logo','affiche_image']
+    list_per_page = 10
+    actions = ('active','desactive')
+    ordering = ['titre']
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés" 
+       
+    def affiche_image(self,obj):
+        return mark_safe('<img src = "{url}" width = " 100px " heigth = " 100px " />'.format(url=obj.image.url))
+    
+    def affiche_logo(self,obj):
+        return mark_safe('<img src = "{url}" width = " 100px " heigth = " 100px " />'.format(url=obj.logo.url))
+    
 
 
 class FooterFrontAdmin(admin.ModelAdmin):
 
     list_display = (
-        'id',
         'titre',
         'about_text',
         'newslater_text',
         'folow_text',
-        'description',
         'status',
         'date_add',
         'date_upd',
@@ -116,39 +129,57 @@ class FooterFrontAdmin(admin.ModelAdmin):
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'titre',
-        'about_text',
-        'newslater_text',
-        'folow_text',
-        'description',
-        'status',
-        'date_add',
-        'date_upd',
     )
-
+    search_fields = ('titre',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['titre']
+    list_per_page = 10
+    actions = ('active','desactive')
+    ordering = ['titre']
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
 
 class SocialAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'name', 'lien', 'status', 'date_add', 'date_upd')
+    list_display = ('name', 'lien', 'status', 'date_add', 'date_upd')
     list_filter = (
-        'status',
-        'date_add',
-        'date_upd',
-        'id',
-        'name',
-        'lien',
         'status',
         'date_add',
         'date_upd',
     )
     search_fields = ('name',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['name']
+    list_per_page = 10
+    actions = ('active','desactive')
+    ordering = ['name']
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
 
 
 class LocationMapAdmin(admin.ModelAdmin):
 
     list_display = (
-        'id',
         'map',
         'latitude',
         'longitude',
@@ -160,44 +191,81 @@ class LocationMapAdmin(admin.ModelAdmin):
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'map',
-        'latitude',
-        'longitude',
-        'status',
-        'date_add',
-        'date_upd',
     )
-
+    date_hierarchy = 'date_add'
+    list_display_links = ['map']
+    list_per_page = 10
+    actions = ('active','desactive')
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
+    
 
 class CopyrightAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'titre', 'status', 'date_add', 'date_upd')
+    list_display = ('titre', 'status', 'date_add', 'date_upd')
     list_filter = (
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'titre',
-        'status',
-        'date_add',
-        'date_upd',
     )
-
+    search_fields = ('titre',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['titre']
+    list_per_page = 10
+    actions = ('active','desactive')
+    ordering = ['titre']
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
 
 class InstagramAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'image', 'status', 'date_add', 'date_upd')
+    list_display = ('affiche_image', 'status', 'date_add', 'date_upd')
     list_filter = (
         'status',
         'date_add',
         'date_upd',
-        'id',
-        'image',
-        'status',
-        'date_add',
-        'date_upd',
     )
+    # search_fields = ('titre',)
+    date_hierarchy = 'date_add'
+    list_display_links = ['affiche_image']
+    list_per_page = 10
+    actions = ('active','desactive')
+    def active(self, request, queryset):
+        queryset.update(status=True)
+        self.message_user(request,"La selection a été activé avec succés")
+        
+    active.short_description = "activer Les configuration selectionnés"
+        
+        
+    def desactive(self, request, queryset):
+        queryset.update(status=False)
+        self.message_user(request,"La selection a été desactivé avec succés")
+            
+    desactive.short_description = "desactivés Les configuration selectionnés"
+    def affiche_image(self,obj):
+        return mark_safe('<img src = "{url}" width = " 100px " heigth = " 100px " />'.format(url=obj.image.url)) 
+
+
 
 
 def _register(model, admin_class):
@@ -205,7 +273,7 @@ def _register(model, admin_class):
 
 
 _register(models.AllInfo, AllInfoAdmin)
-_register(models.workingHours, workingHoursAdmin)
+# _register(models.workingHours, workingHoursAdmin)
 _register(models.HeaderFront, HeaderFrontAdmin)
 _register(models.FooterFront, FooterFrontAdmin)
 _register(models.Social, SocialAdmin)
